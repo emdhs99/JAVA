d563de83676400c5326bce13688bdd48e4ae8af6
deu_on <83750196+emdhs99@users.noreply.github.com>
2023-02-15 18:45:15 +0900

    javafestival3

---
+++ ex03아스키코드.java
@@ -0,0 +1,18 @@
+package javafestival3;
+
+public class ex03아스키코드 {
+
+	public static void main(String[] args) {
+
+//		for(int i = 97; i <= 122; i++ ) {
+//			char ch = (char)i;
+//			System.out.print(ch + " ");
+
+		for (int i = 97; i < 122; i++) {
+			char num = (char) i;
+			System.out.print(num + " ");
+		}
+
+	}
+
+}
